a62cb720094bc246ee920cad921ad67bf9d1f2e7
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-08-21 20:50:43 +0600

    full complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-let 
+let displaydigit = document.getElementById('display')
+    
+ function digitNumber(digit){
+    displaydigit.value = display.value + digit;
+   
+ }
+
+ function digitClear(){
+    displaydigit.value=" ";
+ }
+
+ function digitDlt(){
+    displaydigit.value = displaydigit.value.slice(0,-1)
+ }
+
+ function digitCalc(){
+    let result = displaydigit.value;
+    let calculate = eval(result)
+    displaydigit.value = calculate;
+ }
